76c1e86914d9a113958779a204ff168941f5edf0
Coolman19181 <168024766+Coolman19181@users.noreply.github.com>
2025-02-08 23:20:44 -0500

    Add files via upload

---
+++ neco hax.lua
@@ -0,0 +1,314 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
+local epicdudegui = Instance.new("TextButton")
+local zerogravity = Instance.new("TextButton")
+local touchflinggui = Instance.new("TextButton")
+local c00lguifever = Instance.new("TextButton")
+local infyield = Instance.new("TextButton")
+local waappbtools = Instance.new("TextButton")
+local waappdestroyer = Instance.new("TextButton")
+local waapfebypass = Instance.new("TextButton")
+local print = Instance.new("TextButton")
+local freaky = Instance.new("TextButton")
+local flinggui = Instance.new("TextButton")
+local fighter = Instance.new("TextButton")
+local TextLabel = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+ImageLabel.Parent = ScreenGui
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.623607993, 0, 0.188016534, 0)
+ImageLabel.Size = UDim2.new(0, 293, 0, 283)
+ImageLabel.Image = "rbxassetid://107683872186423"
+
+epicdudegui.Name = "epicdude gui"
+epicdudegui.Parent = ImageLabel
+epicdudegui.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+epicdudegui.BackgroundTransparency = 0.200
+epicdudegui.BorderColor3 = Color3.fromRGB(0, 0, 0)
+epicdudegui.BorderSizePixel = 0
+epicdudegui.Position = UDim2.new(0.0273037534, 0, 0.150128901, 0)
+epicdudegui.Size = UDim2.new(0, 77, 0, 40)
+epicdudegui.Modal = true
+epicdudegui.Font = Enum.Font.SourceSans
+epicdudegui.Text = "epicdude gui"
+epicdudegui.TextColor3 = Color3.fromRGB(0, 0, 0)
+epicdudegui.TextSize = 14.000
+
+zerogravity.Name = "zero gravity"
+zerogravity.Parent = ImageLabel
+zerogravity.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+zerogravity.BackgroundTransparency = 0.200
+zerogravity.BorderColor3 = Color3.fromRGB(0, 0, 0)
+zerogravity.BorderSizePixel = 0
+zerogravity.Position = UDim2.new(0.36555329, 0, 0.150128901, 0)
+zerogravity.Size = UDim2.new(0, 77, 0, 40)
+zerogravity.Font = Enum.Font.SourceSans
+zerogravity.Text = "say no to gravity"
+zerogravity.TextColor3 = Color3.fromRGB(0, 0, 0)
+zerogravity.TextSize = 14.000
+
+touchflinggui.Name = "touch fling gui"
+touchflinggui.Parent = ImageLabel
+touchflinggui.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+touchflinggui.BackgroundTransparency = 0.200
+touchflinggui.BorderColor3 = Color3.fromRGB(0, 0, 0)
+touchflinggui.BorderSizePixel = 0
+touchflinggui.Position = UDim2.new(0.710263193, 0, 0.150128901, 0)
+touchflinggui.Size = UDim2.new(0, 77, 0, 40)
+touchflinggui.Font = Enum.Font.SourceSans
+touchflinggui.Text = "touch fling gui"
+touchflinggui.TextColor3 = Color3.fromRGB(0, 0, 0)
+touchflinggui.TextSize = 14.000
+
+c00lguifever.Name = "c00lgui fe ver"
+c00lguifever.Parent = ImageLabel
+c00lguifever.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+c00lguifever.BackgroundTransparency = 0.200
+c00lguifever.BorderColor3 = Color3.fromRGB(0, 0, 0)
+c00lguifever.BorderSizePixel = 0
+c00lguifever.Position = UDim2.new(0.0276693404, 0, 0.330340922, 0)
+c00lguifever.Size = UDim2.new(0, 77, 0, 40)
+c00lguifever.Font = Enum.Font.SourceSans
+c00lguifever.Text = "c00lgui fe version"
+c00lguifever.TextColor3 = Color3.fromRGB(0, 0, 0)
+c00lguifever.TextSize = 14.000
+
+infyield.Name = "inf yield"
+infyield.Parent = ImageLabel
+infyield.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+infyield.BackgroundTransparency = 0.200
+infyield.BorderColor3 = Color3.fromRGB(0, 0, 0)
+infyield.BorderSizePixel = 0
+infyield.Position = UDim2.new(0.36555329, 0, 0.330340922, 0)
+infyield.Size = UDim2.new(0, 77, 0, 40)
+infyield.Font = Enum.Font.SourceSans
+infyield.Text = "Infinite Yield"
+infyield.TextColor3 = Color3.fromRGB(0, 0, 0)
+infyield.TextSize = 14.000
+
+waappbtools.Name = "waapp btools"
+waappbtools.Parent = ImageLabel
+waappbtools.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+waappbtools.BackgroundTransparency = 0.200
+waappbtools.BorderColor3 = Color3.fromRGB(0, 0, 0)
+waappbtools.BorderSizePixel = 0
+waappbtools.Position = UDim2.new(0.710263193, 0, 0.330340922, 0)
+waappbtools.Size = UDim2.new(0, 77, 0, 40)
+waappbtools.Font = Enum.Font.SourceSans
+waappbtools.Text = "waapp btools"
+waappbtools.TextColor3 = Color3.fromRGB(0, 0, 0)
+waappbtools.TextSize = 14.000
+
+waappdestroyer.Name = "waapp destroyer"
+waappdestroyer.Parent = ImageLabel
+waappdestroyer.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+waappdestroyer.BackgroundTransparency = 0.300
+waappdestroyer.BorderColor3 = Color3.fromRGB(0, 0, 0)
+waappdestroyer.BorderSizePixel = 0
+waappdestroyer.Position = UDim2.new(0.0276693404, 0, 0.524687231, 0)
+waappdestroyer.Size = UDim2.new(0, 77, 0, 40)
+waappdestroyer.Font = Enum.Font.SourceSans
+waappdestroyer.Text = "waapp fucker"
+waappdestroyer.TextColor3 = Color3.fromRGB(0, 0, 0)
+waappdestroyer.TextSize = 14.000
+
+waapfebypass.Name = "waap fe bypass"
+waapfebypass.Parent = ImageLabel
+waapfebypass.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+waapfebypass.BackgroundTransparency = 0.200
+waapfebypass.BorderColor3 = Color3.fromRGB(0, 0, 0)
+waapfebypass.BorderSizePixel = 0
+waapfebypass.Position = UDim2.new(0.36555329, 0, 0.524687231, 0)
+waapfebypass.Size = UDim2.new(0, 77, 0, 40)
+waapfebypass.Font = Enum.Font.SourceSans
+waapfebypass.Text = "waapp FE bypass"
+waapfebypass.TextColor3 = Color3.fromRGB(0, 0, 0)
+waapfebypass.TextSize = 14.000
+
+print.Name = "print"
+print.Parent = ImageLabel
+print.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+print.BackgroundTransparency = 0.200
+print.BorderColor3 = Color3.fromRGB(0, 0, 0)
+print.BorderSizePixel = 0
+print.Position = UDim2.new(0.710263193, 0, 0.524687231, 0)
+print.Size = UDim2.new(0, 77, 0, 40)
+print.Font = Enum.Font.SourceSans
+print.Text = "print ig"
+print.TextColor3 = Color3.fromRGB(0, 0, 0)
+print.TextSize = 14.000
+
+freaky.Name = "freaky"
+freaky.Parent = ImageLabel
+freaky.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+freaky.BackgroundTransparency = 0.200
+freaky.BorderColor3 = Color3.fromRGB(0, 0, 0)
+freaky.BorderSizePixel = 0
+freaky.Position = UDim2.new(0.0276693404, 0, 0.708432794, 0)
+freaky.Size = UDim2.new(0, 77, 0, 40)
+freaky.Font = Enum.Font.SourceSans
+freaky.Text = "freaky gui"
+freaky.TextColor3 = Color3.fromRGB(0, 0, 0)
+freaky.TextSize = 14.000
+
+flinggui.Name = "fling gui"
+flinggui.Parent = ImageLabel
+flinggui.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+flinggui.BackgroundTransparency = 0.200
+flinggui.BorderColor3 = Color3.fromRGB(0, 0, 0)
+flinggui.BorderSizePixel = 0
+flinggui.Position = UDim2.new(0.36555329, 0, 0.708432794, 0)
+flinggui.Size = UDim2.new(0, 77, 0, 40)
+flinggui.Font = Enum.Font.SourceSans
+flinggui.Text = "fling gui"
+flinggui.TextColor3 = Color3.fromRGB(0, 0, 0)
+flinggui.TextSize = 14.000
+
+fighter.Name = "fighter"
+fighter.Parent = ImageLabel
+fighter.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+fighter.BackgroundTransparency = 0.200
+fighter.BorderColor3 = Color3.fromRGB(0, 0, 0)
+fighter.BorderSizePixel = 0
+fighter.Position = UDim2.new(0.710263193, 0, 0.708432794, 0)
+fighter.Size = UDim2.new(0, 77, 0, 40)
+fighter.Font = Enum.Font.SourceSans
+fighter.Text = "FE fighter"
+fighter.TextColor3 = Color3.fromRGB(0, 0, 0)
+fighter.TextSize = 14.000
+
+TextLabel.Parent = ImageLabel
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 0.300
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(-0.000965313695, 0, 0.000737382332, 0)
+TextLabel.Size = UDim2.new(0, 293, 0, 35)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Neco Hax"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 14.000
+
+-- Scripts:
+
+local function DISO_fake_script() -- epicdudegui.Script 
+	local script = Instance.new('Script', epicdudegui)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Coolman19181/epdude-f-e-gu-i/refs/heads/main/epdude%20fe%20gui", true))()
+	end)
+end
+coroutine.wrap(DISO_fake_script)()
+local function HOOWRUD_fake_script() -- zerogravity.Script 
+	local script = Instance.new('Script', zerogravity)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Bac0nHck/Scripts/refs/heads/main/zerogravity"))()
+	end)
+end
+coroutine.wrap(HOOWRUD_fake_script)()
+local function VOUWJLS_fake_script() -- touchflinggui.Script 
+	local script = Instance.new('Script', touchflinggui)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/ai4aYChF"))()
+	end)
+end
+coroutine.wrap(VOUWJLS_fake_script)()
+local function KNVBUHY_fake_script() -- c00lguifever.Script 
+	local script = Instance.new('Script', c00lguifever)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:GetObjects("rbxassetid://17760975806")[1].Source)()
+	
+	
+		local player = game.Players.LocalPlayer
+		local gui = player.PlayerGui:WaitForChild("CoolGui")
+		local cope = gui:WaitForChild("cope")
+		local title = gui:WaitForChild("main"):WaitForChild("title")
+	
+	
+		cope.Position = UDim2.new(0, 500, 0, 298.5)
+		cope.Size = UDim2.new(0, 100, 0, 20)
+	
+	
+		gui.IgnoreGuiInset = true
+		title.Text = "c00lgui revival mobile edition"
+	end)
+end
+coroutine.wrap(KNVBUHY_fake_script)()
+local function MMVE_fake_script() -- infyield.Script 
+	local script = Instance.new('Script', infyield)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Tobiaszeqq/RobloxscriptsArchive/refs/heads/main/2021-InfiniteYield.lua"))()
+	end)
+end
+coroutine.wrap(MMVE_fake_script)()
+local function MNXPO_fake_script() -- waappbtools.Script 
+	local script = Instance.new('Script', waappbtools)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/9auXKEaY"))()
+	end)
+end
+coroutine.wrap(MNXPO_fake_script)()
+local function TWFBJZF_fake_script() -- waappdestroyer.Script 
+	local script = Instance.new('Script', waappdestroyer)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/BywSVje6"))()
+	end)
+end
+coroutine.wrap(TWFBJZF_fake_script)()
+local function UCUHUKP_fake_script() -- waapfebypass.Script 
+	local script = Instance.new('Script', waapfebypass)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		print("sike")
+	end)
+end
+coroutine.wrap(UCUHUKP_fake_script)()
+local function TIGJHK_fake_script() -- print.Script 
+	local script = Instance.new('Script', print)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		print("neco hax by tehpwnzerlolz")
+	end)
+end
+coroutine.wrap(TIGJHK_fake_script)()
+local function OLOYK_fake_script() -- freaky.Script 
+	local script = Instance.new('Script', freaky)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/ShutUpJamesTheLoser/freaky/refs/heads/main/fe", true))()
+	end)
+end
+coroutine.wrap(OLOYK_fake_script)()
+local function QYHB_fake_script() -- flinggui.Script 
+	local script = Instance.new('Script', flinggui)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Orangecatgamer696969/Fling-gui/main/README.md", true)) ()
+	end)
+end
+coroutine.wrap(QYHB_fake_script)()
+local function ZIPS_fake_script() -- fighter.Script 
+	local script = Instance.new('Script', fighter)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		loadstring(game:HttpGet("https://pastefy.app/wxVAgZpT/raw"))()
+	end)
+end
+coroutine.wrap(ZIPS_fake_script)()
